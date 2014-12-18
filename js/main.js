595e954453b26bb0e1c6905edca229fedf145f99
Andy Hoffman <antibland@gmail.com>
2014-12-18 00:42:48 -0500

    nav links should only respond to click events

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ var main = (function() {
         ESC            = 27;
 
     [].forEach.call(nav_links, function(el) {
-      el.addEventListener("touchend", function(e) {
+      el.addEventListener("click", function(e) {
         closeMenu(true);
       });
     });
